bcc4fe1c2ed2b054066c33ed1a1043ebdabcd66f
Barrilli01 <gabrielbarrillicruz@gmail.com>
2024-06-19 13:24:05 -0300

    Criação e configuração inicial do projeto

---
+++ src/main/java/org/gabrielbarrilli/financeirocontrol/Configuration/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package org.gabrielbarrilli.financeirocontrol.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI();
+    }
+}
